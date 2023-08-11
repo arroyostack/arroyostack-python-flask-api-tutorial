fb056ddd6cd87c1939de69053a3ca10c412200a4
El Hadj Mbengue Arroyo <109061270+arroyostack@users.noreply.github.com>
2023-08-11 10:11:07 +0000

    Assesment

--- src/app.py
+++ src/app.py
@@ -1,36 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
-from flask import request
 
 
-todos = [
-    { "label": "My first task", "done": False },
-    { "label": "My second task", "done": False }
-]
+@app.route('/blabla', methods=['GET'])
+def hello_world():
+    return 'Hello, World!'
 
 
-
-
+todos = [{ "label": "My first task", "done": False }]
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
+def hello_world(): 
     json_text = jsonify(todos)
+    return  json_text
 
-  
-
-    return json_text
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.data.json
-    todos.append(json_text)
+    request_body = request.json
+    todos.append(request_body)
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    return jsonify(todos)
 
 
-
-
-
-# These two lines should always be at the end of your app.py file.
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ", position)
+    if position < len(todos):
+        del todos[position]
+        return 'Se elimino correctamente' 
+    else:    
+        return 'Error de rango'
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
